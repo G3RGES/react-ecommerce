c956c613fb3f3e8e0db24040a5ae448a04e3c5a7
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-05-15 08:04:02 +0300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,7 +15,7 @@ const Home = () => {
         useContext + useReducer store
       </h1>
       <h2 className="text-4xl tracking-wide py-4 pl-8">New in</h2>
-      <div className="flex flex-col items-center lg:grid lg:grid-cols-3 gap-4">
+      <div className="flex flex-col items-center lg:grid lg:grid-cols-3 gap-4 px-8">
         {storeData.map((item, idx) => (
           <Product key={idx} item={item} />
         ))}
