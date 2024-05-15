539dbaa25cd4fa4a521c3bb231bb5eb25960c8ef
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-05-15 10:49:46 +0300

    Update Basket.js

--- src/pages/Basket.js
+++ src/pages/Basket.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import Navbar from "../components/Navbar";
+import { StoreContext } from "../context-and-reducer/StoreContext";
+import BasketProduct from "../components/BasketProduct";
 
 const Basket = () => {
+  const { products, total } = useContext(StoreContext);
+
   return (
-    <div>
+    <div className="w-full max-w-3xl mx-auto">
       <Navbar />
+      <div className="flex flex-row items-center justify-between mt-2 py-6 px-10 text-xl font-medium">
+        <h3 className="">Your Basket</h3>
+        <p className="">Total:${total} </p>
+      </div>
+
+      {products.map((product, idx) => (
+        <BasketProduct key={idx} item={product} />
+      ))}
     </div>
   );
 };
