d2172f6a1c0d139783c24d2464a6b72f0211371b
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-05-15 08:01:58 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
+    <div className="flex flex-row items-center justify-evenly mt-2 pt-3 text-lg font-medium">
       <Link to="/">Home</Link>
       <Link to="basket">Basket</Link>
     </div>
