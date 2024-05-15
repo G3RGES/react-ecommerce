04f5216791012cf286377e1d2304c415be76ddea
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-05-15 11:00:34 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { StoreContext } from "../context-and-reducer/StoreContext";
 
 const Navbar = () => {
+  const { products } = useContext(StoreContext);
+
   return (
     <>
       <div className="flex flex-row items-center justify-evenly mt-2 pt-3 text-lg font-medium">
         <Link to="/">Home</Link>
-        <Link to="basket">Basket</Link>
+        <Link to="basket">
+          Basket
+          <span className="text-xs align-top rounded-full px-2 py-1 mx-1 bg-red-500 text-white">
+            {" "}
+            {products.length}
+          </span>
+        </Link>
       </div>
       <h1 className="text-3xl text-center font-semibold mt-2 py-4">
         useContext + useReducer store
