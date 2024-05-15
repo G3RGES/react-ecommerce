5b7a949a56a50184c76e38ce10724c825aecfb1e
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-05-15 07:30:34 +0300

    Create Data.js

---
+++ src/Data.js
@@ -0,0 +1,31 @@
+export const storeData = [
+  {
+    name: "Aviator Sunglasses",
+    price: 50,
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/61UXx%2Bs33YL._SS400_.jpg",
+  },
+  {
+    name: "Cat-Eye Sunglasses",
+    price: 75,
+    image:
+      "https://img.kwcdn.com/product/open/2023-11-26/1701011269957-b4d577727a0444d9b74b27385889fe88-goods.jpeg?imageMogr2/auto-orient%7CimageView2/2/w/800/q/70/format/webp",
+  },
+  {
+    name: "Round Sunglasses",
+    price: 40,
+    image:
+      "https://img.kwcdn.com/product/fancy/53a25832-f974-4d94-9e73-09fc6dd717b8.jpg?imageMogr2/auto-orient%7CimageView2/2/w/800/q/70/format/webp",
+  },
+  {
+    name: "Wayfarer Sunglasses",
+    price: 60,
+    image:
+      "https://images2.ray-ban.com//cdn-record-files-pi/9ec26c0f-3ff5-4a70-9a55-b07c006d58d7/d1a70e34-6d2a-4988-8b89-b07c006d5bd7/0RW4006__601_71__P21__shad__qt.png?impolicy=RB_Product_clone&width=800&bgc=%23f2f2f2",
+  },
+  {
+    name: "Pilot Sunglasses",
+    price: 80,
+    image: "https://m.media-amazon.com/images/I/41JMqLoaAsL._AC_SY580_.jpg",
+  },
+];
