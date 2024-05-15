b2c2d6139449f6a72d28c1b5e03df304c5ef921f
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-05-15 09:59:30 +0300

    Update Product.jsx

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
+import { StoreContext } from "../context-and-reducer/StoreContext";
 
 const Product = ({ item }) => {
+  const { addToBasket } = useContext(StoreContext);
+
+  const handleAdd = () => {
+    addToBasket(item);
+  };
+
   return (
     <div className="mx-auto px-8 border border-gray-200 rounded-lg">
       <img className="" src={item.image} alt={item.name} />
@@ -9,7 +16,11 @@ const Product = ({ item }) => {
           <p className="text-xl ">{item.name}</p>
           <p className="text-lg">${item.price}</p>
         </div>
-        <button className="bg-indigo-500 text-white text-lg font-medium py-2 px-2 rounded-md">
+        <button
+          className="bg-indigo-500 text-white 
+        text-lg font-medium py-2 px-2 rounded-md"
+          onClick={handleAdd}
+        >
           Add to Basket
         </button>
       </div>
