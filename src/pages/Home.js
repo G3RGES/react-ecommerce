0bbe059b551eb1321d5630616487076c269b7e38
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-05-15 09:59:36 +0300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,9 +11,7 @@ const Home = () => {
   return (
     <div>
       <Navbar />
-      <h1 className="text-3xl text-center font-semibold mt-2 py-4">
-        useContext + useReducer store
-      </h1>
+
       <h2 className="text-4xl tracking-wide py-4 pl-8">New in</h2>
       <div className="flex flex-col items-center lg:grid lg:grid-cols-3 gap-4 px-8">
         {storeData.map((item, idx) => (
